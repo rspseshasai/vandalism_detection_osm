a9dfd9588a8231ed78a9ce326a33e921810ad9ec
rspseshasai <rspseshasai@gmail.com>
2024-09-17 15:21:59 +0200

    refactor: rename and refactor

--- filter_contributions_by_changeset_id_s3.py
+++ filter_contributions_by_changeset_id_s3.py
@@ -10,21 +10,19 @@ from contribution_schema import get_osm_contribution_schema
 from logger_config import logger
 from s3clientmanager import S3ClientManager
 
-# Load the changeset labels from a TSV file
-labels_file = 'data/ovid_labels.tsv'
-labels_df = pd.read_csv(labels_file, sep='\t')
+vandalism_labels_with_changeset_ids_file = 'data/ovid_labels.tsv'
+labels_df = pd.read_csv(vandalism_labels_with_changeset_ids_file, sep='\t')
 
-# Create a dictionary of changeset IDs and corresponding labels
 changeset_labels = dict(zip(labels_df['changeset'], labels_df['label']))
-changeset_ids_to_filter = list(labels_df['changeset'])
+labelled_changeset_ids_to_filter = list(labels_df['changeset'])
 
 # DataFrame to hold all matching contributions
-filtered_contributions = pd.DataFrame()
+labelled_contributions_df = pd.DataFrame()
 
 # Save threshold (number of S3 objects processed to trigger saving)
-s3_object_threshold = 4  # Save after processing 10 S3 objects
-processed_objects_count = 0  # Counter for processed S3 objects
-file_counter = 0  # Counter to keep track of saved files
+s3_object_threshold = 4
+processed_objects_count = 0
+file_counter = 0
 
 # Define the schema according to the given structure
 schema = get_osm_contribution_schema()
@@ -35,29 +33,20 @@ def save_filtered_contributions(filtered_df, file_counter, schema):
     Save the filtered contributions to a Parquet file with the defined schema.
     """
     output_file = f'output/filtered_contributions_part_{file_counter}.parquet'
-
-    # Convert Pandas DataFrame to Arrow Table with schema enforcement
     table = pa.Table.from_pandas(filtered_df, schema=schema, preserve_index=False)
-
-    # Save the Arrow Table to a Parquet file
     pq.write_table(table, output_file)
     logger.info(f"Filtered contributions saved to {output_file}")
 
 
-def load_and_filter(path, changeset_ids, s3_client, bucket_name):
+def load_and_filter_contributions_with_changeset_id(path, changeset_ids, s3_client, bucket_name):
     """
     Load a Parquet file from S3 and return rows where `changeset.id` is in the specified list.
     """
     with tempfile.NamedTemporaryFile(suffix=".parquet", delete=False) as temp_file:
         temp_file_path = temp_file.name
 
-    # Download the parquet file from S3 to the temporary file
     s3_client.download_file(bucket_name, path, temp_file_path)
-
-    # Load the parquet file into a DataFrame
     contribution_df = pd.read_parquet(temp_file_path, engine='pyarrow')
-
-    # Remove the temporary file
     os.remove(temp_file_path)
 
     # Filter rows where the changeset.id is in the provided list
@@ -88,37 +77,30 @@ while True:
         for obj in tqdm(response['Contents']):
             obj_key = obj['Key']
             if obj_key.endswith('.parquet'):
-                # Load and filter each Parquet file for matching contributions
-                filtered_df = load_and_filter(obj_key, changeset_ids_to_filter, s3_client_manager.s3_client,
-                                              s3_client_manager.bucket_name)
+                filtered_df = load_and_filter_contributions_with_changeset_id(obj_key, labelled_changeset_ids_to_filter,
+                                                                              s3_client_manager.s3_client,
+                                                                              s3_client_manager.bucket_name)
 
                 # Add the corresponding label to the filtered contributions
                 if not filtered_df.empty:
                     filtered_df['vandalism'] = filtered_df['changeset'].apply(
                         lambda x: changeset_labels.get(x['id'], 'unknown')
                     )
-
-                    # Append filtered data to the main DataFrame
-                    filtered_contributions = pd.concat([filtered_contributions, filtered_df], ignore_index=True)
-
-                # Increment the processed objects counter
+                    labelled_contributions_df = pd.concat([labelled_contributions_df, filtered_df], ignore_index=True)
                 processed_objects_count += 1
 
-                # If processed_objects_count exceeds the threshold, save the DataFrame and reset
                 if processed_objects_count >= s3_object_threshold:
-                    save_filtered_contributions(filtered_contributions, file_counter, schema)
+                    save_filtered_contributions(labelled_contributions_df, file_counter, schema)
                     file_counter += 1
-                    processed_objects_count = 0  # Reset counter after saving
-                    filtered_contributions = pd.DataFrame()  # Reset DataFrame after saving
+                    processed_objects_count = 0
+                    labelled_contributions_df = pd.DataFrame()
 
-    # Check if there are more objects to process
     if response.get('IsTruncated'):
         continuation_token = response['NextContinuationToken']
     else:
         break
 
-# Save any remaining contributions that weren't saved due to not meeting the object threshold
-if not filtered_contributions.empty:
-    save_filtered_contributions(filtered_contributions, file_counter, schema)
+if not labelled_contributions_df.empty:
+    save_filtered_contributions(labelled_contributions_df, file_counter, schema)
 
 logger.info(f"Total number of matching contributions saved in multiple files.")
