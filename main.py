a45449d7b4ed1e1a5e33df6583a350a49860fb96
Pavan <rspseshasai@gmail.com>
2024-11-21 18:08:25 +0100

    refactor

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from data_gathering.load_parquet_data import load
 from feature_extraction import get_or_generate_features
 
-contributions_df = load('data/contribution_data/osm_labelled_contributions_v2.parquet', False)
 
-# Extract features for all contributions and store them in a DataFrame
-features_df = get_or_generate_features(contributions_df, True)
+def main():
+    contributions_df = load('data/contribution_data/osm_labelled_contributions_v2.parquet', False)
+
+    # Extract features for all contributions and store them in a DataFrame
+    features_df = get_or_generate_features(contributions_df, True)
+
+
+if __name__ == '__main__':
+    main()
